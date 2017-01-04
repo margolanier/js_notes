8909117d6044f19b63e51a3e6f0308fe9b9ff6c7
Margo Lanier <lanier.margo@gmail.com>
2017-01-03 23:16:26 -0500

    added starter kit for project with gulp, mustache, sass, foundation

---
+++ 00_outline_2.0/app.js
@@ -0,0 +1,5 @@
+function init() {
+	console.log('js working');
+}
+
+window.addEventListener('load', init);
---
+++ 00_outline_2.0/gulpfile.js
@@ -0,0 +1,36 @@
+// Import Gulp
+let gulp = require('gulp');
+
+// Import Gulp Plugins
+let sass = require('gulp-sass'); // download this npm plugin
+
+// Default Task
+gulp.task('default', ['html', 'css', 'js', 'watch']);
+
+// Subtasks
+gulp.task('html', function() {
+	// copy index.html into the public/ directory
+	return gulp.src('index.html')
+		.pipe(gulp.dest('public/'));
+})
+
+gulp.task('css', function() {
+	// convert style.scss into style.css
+	// copy to public/
+	return gulp.src('scss/style.scss')
+		.pipe(sass()) // requires gulp-sass
+		.pipe(gulp.dest('public/'));
+})
+
+gulp.task('js', function() {
+	// copy js file into public/
+	return gulp.src('app.js')
+		.pipe(gulp.dest('public/'));
+})
+
+// Watch files for changes
+gulp.task('watch', function() {
+	gulp.watch('index.html', ['html']);
+	gulp.watch('scss/*.scss', ['css']);
+    gulp.watch('app.js', ['js']);
+});
