732fed322f85c4aabe845d2a3e338d0b028b3461
Margo Lanier <lanier.margo@gmail.com>
2017-01-25 15:33:48 -0500

    added a few more demo functions to app.  made a service.

--- 28_angular/js/app.js
+++ 28_angular/js/app.js
@@ -8,9 +8,12 @@ const app = angular.module('HairApp', []);
  */
 
 
-// Define the controller ('NameOfController, function() { what controller does })
+// Define the controller ('NameOfController, function(params) { what controller does })
 app.controller('OwnerController', function ($scope) {
 	/*
+	 * Scopes are independent across controllers
+	 * ($scope of OwnerController NOT equal to $scope of ClientController)
+	 *
 	 * Can add any number of params to function
 	 * Parameters are 'services' (can use built-in services or customizable)
 	 * - $scope is DOM things
@@ -32,69 +35,46 @@ app.controller('ClientController', function ($scope, HairService) {
 	 * 'HairService' refers to the service created below
 	 */
 	$scope.clientName = '';
+	
+	// Note: 'things' is a local variable
+	// (not accesssible to template unless you add it as property of $scope)
+	let things = ['thingA', 'thingB', 'thingC'];
+	$scope.items = things;
+	
+	// Add client requests to array of tasks
+	// Need tasks[] to be accessible in HTML to loop over list & render
+	
+	// METHOD 1
+	$scope.tasks = ['hair cut'];
+	$scope.addRequest = function(newTask) {
+		$scope.tasks.push(newTask);
+		$scope.clientRequest = ''; // clear textbox content
+	};
+	
+	// METHOD 2
+	$scope.addRequest2 = function() {
+		$scope.tasks.push($scope.clientRequest);
+		$scope.clientRequest = ''; // clear textbox content
+		console.log($scope.tasks);
+	};
+	
+	$scope.remove = function(id) {
+		$scope.tasks.splice(id, 1);
+	};
 });
 
-/**
- * Scopes are INDEPENDENT across controllers. The scope in
- * one controller is not the same as the scope in another
- * controller.
- */
-// Without services
-// app.controller('ListOPetsController', function ($scope) {
-//     $scope.petName = '';
-//     $scope.pets = [];
 
-//     // You won't have access to 'nums' in your template
-//     // because its a local variable. You need to add it
-//     // to scope, so when we do $scope.x that means our
-//     // templates can access this array called 'x'.
-//     let nums = [1, 2, 67];
-//     $scope.x = nums;
-
-//     $scope.addPet = function () {
-//         $scope.pets.push($scope.petName);
-//         $scope.petName = '';
-//     };
-
-//     $scope.remove = function (id) {
-//         $scope.pets.splice(id, 1);
-//     };
-// });
-
-// Second param is name of service that was created below.
-/*app.controller('ListOPetsController', function ($scope, HairService) {
-    $scope.petName = '';
-    $scope.pets = HairService.getPets();
-    // $scope.pets = [];
-
-    $scope.newPet = function () {
-        HairService.addPet($scope.petName);
-        // $scope.pets.push($scope.petName);
-        $scope.petName = '';
-    };
-
-    $scope.remove = function (id) {
-        $scope.pets.splice(id, 1);
-    };
-});*/
-
-/**
- * .factory is how we create services. Sorry but it
- * is, nothing we can do about it.
- * 
- * This function runs once at the beginning and returns
- * the OBJECT THAT IS THE SERVICE.
- */
+// Create services (for data)
+// Function runs once and always returns an **object that is the service**
 app.factory('HairService', function() {
-    let pets = [];
-
+    let tasks = [];
+	
     return {
-        // Made up function names
-        addPet: function (name) {
-            pets.push(name);
+        addRequest: function(task) {
+            tasks.push(task);
         },
-        getPets: function () {
-            return pets;
+        getRequests: function() {
+            return tasks;
         },
     };
 });
