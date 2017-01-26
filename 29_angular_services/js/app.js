900f1d41ce818beee9d059c3fb24810c0a38cc7e
Margo Lanier <lanier.margo@gmail.com>
2017-01-26 10:45:36 -0500

    edited some comments

--- 29_angular_services/js/app.js
+++ 29_angular_services/js/app.js
@@ -1,9 +1,8 @@
 /**
- * Services are the models of Angular. All data that isn't specific to
- * a single controller should be stored in the service.
+ * All data that isn't specific to a single controller should be stored in the service.
  * 
- * Just because its CURRENTLY only used in one controller doesn't mean
- * its EVER only going to be used in one controller.
+ * Just because its CURRENTLY only used in one controller
+ * doesn't mean its EVER only going to be used in one controller.
  */
 
 const app = angular.module('BestBookApp', []);
@@ -13,27 +12,24 @@ function Book(title, author, price) {
     this.title = title;
     this.author = author;
     this.price = price;
-
     this.cover = null;
     this.id = null;
     this.added = null;
-
     this.isFavorite = false;
-
+	
     return this;
 }
 
 app.controller('NewBookController', function ($scope, BookService) {
-    // These are basically the default values for our $scope variables. Also
-    // a convenient list of all the properties we have in our templates.
-    $scope.title = 'lord of the flies';
+    // Default values for our $scope variables
+	// Also a convenient list of all the properties we have in our templates
+    $scope.title = '';
     $scope.author = '';
     $scope.price = 0;
 
     $scope.newBook = function () {
-        // Need to use $scope because that's the name of the strings I want to use as input.
+        // Need to use $scope because that's the name of the strings I want to use as input
         const brandNewBook = new Book($scope.title, $scope.author, $scope.price);
-        // Add is a name I made up -- could have called it anything.
         BookService.add(brandNewBook);
 
         $scope.title = '';
@@ -43,7 +39,7 @@ app.controller('NewBookController', function ($scope, BookService) {
 });
 
 app.controller('ShowBooksController', function ($scope, BookService) {
-    // Get the array at the beginning so we can render it right away.
+    // Get the array at the beginning so we can render it right away
     $scope.books = BookService.getAll();
 
     $scope.favoriteBook = function (target) {
@@ -52,11 +48,10 @@ app.controller('ShowBooksController', function ($scope, BookService) {
     };
 });
 
-// TODO: make a service to store books
-// Factories always return services.
+// Factories always return services
 app.factory('BookService', function () {
-    // Great example of a closure - books is preserved throughout
-    // the lifetime of our app.
+    // Great example of a closure
+	// books is preserved throughout the lifetime of our app
     const books = [];
 
     books.push(new Book('test book', 'famous author', 17.89));
